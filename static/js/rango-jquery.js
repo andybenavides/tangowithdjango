2b5b523100ff39fd4da0594488091c8e3c0b4cb8
Andy Benavides <andybenavides.22@gmail.com>
2016-03-07 22:28:15 -0800

    Added add page functionality from search and ajax implementation for category search and likes/views

--- static/js/rango-jquery.js
+++ static/js/rango-jquery.js
@@ -1,12 +1,5 @@
 $(document).ready( function() {
 
-    $("p").hover( function() {
-            $(this).css('color', 'red');
-    },
-    function() {
-            $(this).css('color', 'blue');
-    });
-
     $("#about-btn").click( function(event) {
         msgstr = $("#msg").html()
         msgstr = msgstr + "ooooooooooo"
